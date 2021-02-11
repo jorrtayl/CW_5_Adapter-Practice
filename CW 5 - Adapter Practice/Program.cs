12a6eaddc5fae90aaa009ffd0b7709b2f88e52ba
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-02-11 15:15:53 -0600

    Add project files.

---
+++ CW 5 - Adapter Practice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW_5___Adapter_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
