d2f0c94f35302b42ef34308c3c9081c62797f5c5
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-02-11 16:16:52 -0600

    Finished assignment.

--- CW 5 - Adapter Practice/Program.cs
+++ CW 5 - Adapter Practice/Program.cs
@@ -6,10 +6,77 @@ using System.Threading.Tasks;
 
 namespace CW_5___Adapter_Practice
 {
+    class TeddyBear : IToyBear
+    {
+        public void hug()
+        {
+            Console.WriteLine("Hugs you! :)");
+        }
+    }
+
+    class Grizzly : IBear
+    {
+        void IBear.hibernate()
+        {
+            Console.WriteLine("Goes to sleep.. Zzzzzzz..");
+        }
+
+        void IBear.maul()
+        {
+            Console.WriteLine("Mauls you! >:D");
+        }
+    }
+
+    class BearAdapter : IToyBear
+    {
+        private IBear bear;
+
+        public BearAdapter(IBear b)
+        {
+            this.bear = b;
+        }
+
+        public void hug()
+        {
+            bear.maul();
+        }
+    }
+
+    interface IToyBear
+    {
+        void hug();
+    }
+
+    interface IBear
+    {
+        void hibernate();
+        void maul();
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            IBear bear = new Grizzly();
+            IToyBear toy = new TeddyBear();
+            IToyBear toyAdapt = new BearAdapter(bear);
+
+            Console.WriteLine("Testing with IBear..");
+            bear.hibernate();
+            bear.maul();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Testing with IToyBear..");
+            toy.hug();
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("Testing with IToyBear with BearAdapter..");
+            toyAdapt.hug();
+
+            Console.ReadKey();
         }
     }
 }
