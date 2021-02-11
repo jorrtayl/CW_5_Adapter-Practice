dc43b0ec7c0b495442726f81e3dfc8fbc2644ff2
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-02-11 16:18:11 -0600

    Added header comments.

--- CW 5 - Adapter Practice/Program.cs
+++ CW 5 - Adapter Practice/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * Author: Jordan Taylor
+ * Date: 02 / 11 / 2021
+ * Description: Tests bear creations (toy and real) based on an Adaptor Pattern UML.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
